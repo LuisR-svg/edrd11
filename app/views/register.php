09f9e263ecc219f4e108520b2ba0d1277f7baeb0
LuisR-svg <luis.papito.666@gmail.com>
2025-02-12 13:32:51 -0600

    asd

--- app/views/register.php
+++ app/views/register.php
@@ -1,5 +1,5 @@
 <?php
-require '../modles/connection.php';
+require '../models/connection.php';
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $username = $_POST['username'];
     $password = password_hash($_POST['password'], PASSWORD_BCRYPT);
