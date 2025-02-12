ad208e91c0204aa303ebd15a89276a1d99cbef14
LuisR-svg <luis.papito.666@gmail.com>
2025-02-12 11:50:15 -0600

    index

--- index.php
+++ index.php
@@ -1,1 +1,6 @@
-
+<?php
+<form method="POST">
+<input type="text" name="username" required placeholder="Username">
+<input type="password" name="password" required placeholder="Password">
+<button type="submit">Login</button>
+</form>
