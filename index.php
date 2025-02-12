bc4ed8ca9fe1c01824427c1b530ee6609f3f79b6
LuisR-svg <luis.papito.666@gmail.com>
2025-02-12 16:54:29 -0600

    asdasd

--- index.php
+++ index.php
@@ -3,8 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- <link rel="stylesheet" src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" crossorigin="anonymous"></link> -->
+    <script src="https://kit.fontawesome.com/d4641596a6.js" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="/public/css/main.css">
-    <link rel="stylesheet" src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" crossorigin="anonymous"></link>
     <title>Document</title>
 </head>
 <body class="home">
