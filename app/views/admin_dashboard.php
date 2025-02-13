19d66fbe15017840b39542da198a6bd0090abae9
LuisR-svg <luis.papito.666@gmail.com>
2025-02-13 14:47:19 -0600

    asdasd

--- app/views/admin_dashboard.php
+++ app/views/admin_dashboard.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
 <html lang="en">
-<?php include 'app/helpers/head.php'; ?>
-</head>
+    <?php include 'app/helpers/head.php'; ?>
+    </head>
 <body>
-<?php include 'app/helpers/navBar.php'; ?>
+    <?php include 'app/helpers/navBar.php'; ?>
 </body>
 </html>
