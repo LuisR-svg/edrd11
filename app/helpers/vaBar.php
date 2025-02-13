bb347ad34b5ee4e9f7a00e5ea93f553d52bc801c
LuisR-svg <luis.papito.666@gmail.com>
2025-02-13 14:15:22 -0600

    asds

---
+++ app/helpers/vaBar.php
@@ -0,0 +1,6 @@
+<nav>
+       <ol>
+        <li> <a href="app/controllers/logout.php">Logout</a></li>
+       </ol>
+      <!-- <span><img src="/public/images/compas.gif" alt=""> </span> -->
+</nav>
