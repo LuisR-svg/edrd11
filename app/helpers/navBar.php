e5cff0e7fcccc889ccfed852653214f89e2089ac
LuisR-svg <luis.papito.666@gmail.com>
2025-02-13 15:10:01 -0600

    asd

--- app/helpers/navBar.php
+++ app/helpers/navBar.php
@@ -1,9 +1,10 @@
 <nav>
        <ul>
-        <li> <a href="../controllers/logout.php">Logout</a></li>
+        <li><a href="/" class="active">Home</a></li>        
         <li><a href="#">Payments</a></li>
         <li><a href="#">Events</a></li>
-        <li><a href="#">Reports</a></li>
+        <li><a href="#">Reports</a></li>     
+        <li> <a href="../controllers/logout.php">Logout</a></li> 
        </ul>
       <!-- <span><img src="/public/images/compas.gif" alt=""> </span> -->
 </nav>
