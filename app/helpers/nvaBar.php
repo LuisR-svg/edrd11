da405c36b24943a1d3c669b57be0c295dd6e31e7
LuisR-svg <luis.papito.666@gmail.com>
2025-02-13 14:24:53 -0600

    dasd

--- app/helpers/nvaBar.php
+++ app/helpers/nvaBar.php
@@ -1,6 +1,6 @@
 <nav>
        <ol>
-        <li> <a href="app/controllers/logout.php">Logout</a></li>
+        <li> <a href="../controllers/logout.php">Logout</a></li>
        </ol>
       <!-- <span><img src="/public/images/compas.gif" alt=""> </span> -->
 </nav>
