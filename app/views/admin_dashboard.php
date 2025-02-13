4f66ded87fca62abe0eec0832816faeee4f8ea54
LuisR-svg <luis.papito.666@gmail.com>
2025-02-13 14:45:50 -0600

    asd

--- app/views/admin_dashboard.php
+++ app/views/admin_dashboard.php
@@ -1,11 +1,8 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+<?php include 'app/helpers/head.php'; ?>
 </head>
 <body>
-    <nav></nav>
+<?php include 'app/helpers/navBar.php'; ?>
 </body>
 </html>
