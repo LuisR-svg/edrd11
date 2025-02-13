4d67a8ab95f4251190ce8bde33287ac7f5c3bff2
LuisR-svg <luis.papito.666@gmail.com>
2025-02-13 14:42:54 -0600

    cbnvbn

---
+++ app/helpers/head.php
@@ -0,0 +1,9 @@
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- <link rel="stylesheet" src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" crossorigin="anonymous"></link> -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="../../public/css/main.css">
+    <link rel="stylesheet" href="https://use.typekit.net/inp3ria.css">
+    <title>EDRD11</title>
+    <link rel="icon" type="image/x-icon" href="../../public/images/favicon.ico">
