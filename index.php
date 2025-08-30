4abb0c24c3a6efaf001113a0425bf889531129bb
LuisR-svg <luis.papito.666@gmail.com>
2025-08-30 17:30:21 -0600

    Refactor login form structure and styles for improved layout and user experience

--- index.php
+++ index.php
@@ -1,9 +1,8 @@
-<!DOCTYPE html>
+<!-- <!DOCTYPE html>
 <html lang="en">
-<?php include 'app/helpers/head.php'; ?>
+//
 </head>
-<body class="home-page">
-  
+<body class="home-page">  
     <div class="home">
     <main class="form-container">
         <form method="POST" action="app/controllers/login.php" class="login-form left-side">
@@ -33,5 +32,40 @@
     </main>
    
 </div>
+</body>
+</html> -->
+
+<!DOCTYPE html>
+<html lang="en">
+<?php include 'app/helpers/head.php'; ?>
+</head>
+<body class="home-page">
+<main>
+        <div class="left-side">
+            <form method="POST" action="app/controllers/login.php" class="login-form">
+                    <div class="form-header">
+                        <h1>M<i class="fa-solid fa-compass-drafting"></i>sonic Treasury</h1>
+                        <h2>Estrell<i class="fa-sharp-duotone fa-solid fa-star-of-david"></i> Del Rey David #11</h2>
+                    </div>
+                    <div class="input-container">
+                            <h5>User Name</h5>
+                        <div class="input-box">                 
+                                <input type="text" name="username" required placeholder="Enter your username">
+                                <i class="fa-solid fa-user-tie"></i>
+                        </div>  
+                    </div>
+                    <div class="input-container">
+                           <h5>password</h5>
+                            <div class="input-box">                        
+                                <input type="password" name="password" required placeholder="Enter your password" minlength="3">
+                                <i class="fa-solid fa-lock"></i>
+                            </div>
+                        <button type="submit">Login <span></span> </button>     
+                    </div>    
+            </form>
+        </div>
+    <div class="right-side"></div>
+</main>
+    
 </body>
 </html>
