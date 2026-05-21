848aea29f83a2219995eeb713c928053df4ebcb2
LuisR-svg <luis.papito.666@gmail.com>
2026-05-21 14:50:20 -0600

    ds

--- index.php
+++ index.php
@@ -54,7 +54,7 @@ $showLogin = get_param('login'); // 'member' or 'admin'
   <link rel="icon" type="image/x-icon" href="/assets/img/star-ico.ico">
   <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400;700&family=Inter:wght@300;400;500;600&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css"/>
-  <link rel="stylesheet" href="/assets/css/style.css?v=1.11">
+  <link rel="stylesheet" href="/assets/css/style.css?v=1.12">
 </head>
 <body>
 
